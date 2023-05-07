3d953f03067acb2e9120ca0b49b464ae611f0cc5
Tarun Singh Rawat <126674031+tarunrawatknoldus@users.noreply.github.com>
2023-05-07 14:53:14 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 python3 manage.py makemigrations
 python3 manage.py migrate
+python manage.py syncdb --noinput
+python manage.py createsuperuser --username admin --password admin --email admin@exmaple.com
 python3 manage.py runserver
