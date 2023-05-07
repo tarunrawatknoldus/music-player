58ae8ecba62210c2a27c84d73f824271934c712c
Tarun Singh Rawat <126674031+tarunrawatknoldus@users.noreply.github.com>
2023-05-07 12:14:17 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd martor_demo; python manage.py createsuperuser --no-input)
-fi
-
-cd music-player/music_project; gunicorn wsgi.py --user www-data --bind 0.0.0.0:8010 --workers 3
-
-python3 manage.py runserver 0:8000
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3 manage.py runserver
