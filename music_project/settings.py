91bf69ea025cb49f41904eb609d51051ca7575f9
Tarun Singh Rawat <126674031+tarunrawatknoldus@users.noreply.github.com>
2023-05-07 12:50:31 +0530

    Update settings.py

--- music_project/settings.py
+++ music_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-egh-19yc7o=5pjc3wh=-@3q(!=3+hmm=cj1(x_)*ru98j4rvcb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.49.2','0.0.0.0']
+ALLOWED_HOSTS = ['192.168.49.2','0.0.0.0','127.0.0.1']
 
 
 # Application definition
